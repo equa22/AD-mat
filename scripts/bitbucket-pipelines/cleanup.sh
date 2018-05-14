9409d28c66ab75d0f44b54be24477c8b1e392676
Uros Tement <utement@gmail.com>
2018-05-14 14:37:37 +0200

    Debuging missing folders.

--- scripts/bitbucket-pipelines/cleanup.sh
+++ scripts/bitbucket-pipelines/cleanup.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
 # Remove .git from modules
-ls -la modules/contrib/media_entity_browser/
 rm -Rf modules/contrib/media_entity_browser/.git
-
-ls -la modules/contrib/install_profile_generator/
 rm -Rf modules/contrib/install_profile_generator/.git
-
-ls -la modules/contrib/paragraphs/
 rm -Rf modules/contrib/paragraphs/.git
-
-ls -la modules/contrib/slick_paragraphs/
 rm -Rf modules/contrib/slick_paragraphs/.git
 
 # Remove pipelines
