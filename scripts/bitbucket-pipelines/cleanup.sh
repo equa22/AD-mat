54a7cefbf973872ddcfde488cc548620e1202ba7
Uros Tement <utement@gmail.com>
2018-05-14 12:38:50 +0200

    Debuging missing modules.

--- scripts/bitbucket-pipelines/cleanup.sh
+++ scripts/bitbucket-pipelines/cleanup.sh
@@ -7,7 +7,7 @@ rm -Rf .git
 # Remove .git from modules
 rm -Rf modules/contrib/media_entity_browser/.git
 rm -Rf modules/contrib/install_profile_generator/.git
-rm -Rf  modules/contrib/paragraphs/.git
+rm -Rf modules/contrib/paragraphs/.git
 rm -Rf modules/contrib/slick_paragraphs/.git
 
 # Remove pipelines
