4d73c3a4d9b86ab3b2910e5aaa5294f7b769d5d3
Viktoriia Shnurova <viktoriia.shnurova@agiledrop.com>
2018-05-15 14:59:54 +0200

    JS for main navigation.

--- themes/custom/mat/scripts/app/app.js
+++ themes/custom/mat/scripts/app/app.js
@@ -1,38 +1,60 @@
 (function($) {
-
   'use strict';
 
-  // Mobile menu
-  Drupal.behaviors.opemMobileMenu = {
-    attach: function (context) {
-      var $burger = $('.burger', context);
-      var $body = $('body', context);
-
-      $($burger).on('click', function(event) {
-        $($body).toggleClass('mobile-menu-open');
-        $(this).toggleClass('open');
+  // Toggling visibility of the main navigation (on mobile)
+  Drupal.behaviors.mobileNavigation = {
+    attach: function (context, settings) {
+      $('#header .bars', context).once('mobile-navigation').on('click', function() {
+        $('body').toggleClass('mobile-menu--opened');
       });
     }
   };
 
-  Drupal.behaviors.StickyHeader = {
+  // Accordion for the main navigation (on mobile)
+  Drupal.behaviors.accordionNavigation = {
     attach: function (context, settings) {
-      var $header = $('header');
-      var $body = $('body');
-      var $toolbar = $('#toolbar-bar');
+      function setAccordion() {
+        var windowWidth = $(window).width();
 
-      $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll > 60) {
-          $($header).addClass('sticky');
+        if (windowWidth < 768) {
+          $('#block-mat-main-menu .menu-item--expanded > span, #block-mat-main-menu .menu-item--expanded > a')
+                        .attr('aria-expanded', 'false')
+                        .attr('role', 'button')
+                        .attr('data-toggle', 'collapse')
+                        .addClass('collapsed');
+
+          $('#block-mat-main-menu .menu-item--expanded > ul').addClass('collapse')
+                                 .attr('data-parent', '#block-mat-main-menu');                
+
+          $('#block-mat-main-menu .menu-item--expanded').each(function(n) {
+            $(this).attr('id', 'heading' + n);
+
+            $(this).find('.menu-item--expanded > ul').attr('data-parent', '#heading' + n);
+
+            $(this).find('> span, > a').attr('data-target', '#collapse' + n)
+                                       .attr('aria-controls', 'collapse' + n);
+
+            $(this).find('> ul').attr('id', 'collapse' + n)
+                                .attr('aria-labelledby', 'heading' + n);
+          });
         } else {
-          $($header).removeClass('sticky');
+          $('#block-mat-main-menu .menu-item--expanded > span, .menu-item--expanded > a').attr('data-toggle', '');
         }
+      }
+
+      setAccordion();
+      $(window).on('resize', setAccordion);
+    }
+  };
+
+  // Styling for opened item of the main navigation (on mobile)
+  Drupal.behaviors.openItem = {
+    attach: function (context, settings) {
+      $('#block-mat-main-menu .menu-item--expanded', context).once('opened-item').on('click', function() {
+        $(this).siblings().removeClass('menu-item--opened');
+        $(this).addClass('menu-item--opened');
       });
     }
   };
 
 })(jQuery);
-
-
-
