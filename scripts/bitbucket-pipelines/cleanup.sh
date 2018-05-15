49fa5bd77ae6e9d678a2e3d10222eee9bc8512d6
Uros Tement <utement@gmail.com>
2018-05-15 11:26:33 +0200

    Updated cleanup.sh.

--- scripts/bitbucket-pipelines/cleanup.sh
+++ scripts/bitbucket-pipelines/cleanup.sh
@@ -5,6 +5,7 @@ rm -Rf modules/contrib/media_entity_browser/.git
 rm -Rf modules/contrib/install_profile_generator/.git
 rm -Rf modules/contrib/paragraphs/.git
 rm -Rf modules/contrib/slick_paragraphs/.git
+rm -Rf themes/contrib/adminimal_theme/.git
 
 # Remove pipelines
 rm -rf scripts/bitbucket-pipelines
