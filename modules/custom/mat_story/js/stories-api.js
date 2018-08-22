c41ef5161b708c8afe998e6a2ff493c9b13de9ec
Sara Verhnjak <sara.verhnjak@agiledrop.com>
2018-08-22 11:32:14 +0200

    load stories on new device

--- modules/custom/mat_story/js/stories-api.js
+++ modules/custom/mat_story/js/stories-api.js
@@ -59,8 +59,6 @@
 
   var device = getDevice();
 
-
-
   function checkMobile() {
     /*var check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
@@ -153,6 +151,12 @@
   }
 
   function isOverlapingX(value) {
+    console.log("value: " + value + " X: ", $('.container-small').offset().left,
+       $('.container-small').offset().left + $('.container-small').width())
+
+
+
+
     if(value > $('.container-small').offset().left &&
        value < $('.container-small').offset().left + $('.container-small').width()){
       return true;
@@ -163,7 +167,12 @@
   }
 
   function isOverlapingY(value) {
-    if(value > $('.container-small').offset().top - $('.animation-wrapper').offset().top  - config._el_height &&
+    console.log("value: " + value + " Y: " , $('.container-small').offset().top - $('.animation-wrapper').offset().top  - config._el_height,
+       $('.container-small').offset().top - $('.animation-wrapper').offset().top + $('.container-small').height())
+
+
+
+    if(value > $('.container-small').offset().top - $('.animation-wrapper').offset().top  - config._el_height/2 &&
        value < $('.container-small').offset().top - $('.animation-wrapper').offset().top + $('.container-small').height()) {
       return true;
     }
@@ -233,7 +242,7 @@
 
     checkFilters()
 
-    if(!mobile) {
+    if(getDevice() == 'desktop') {
       stories = sortForSlider(stories);
 
       fadeElementsOut.forEach(function(story, i) {
@@ -250,7 +259,7 @@
 
       elementsToMove.forEach(function(story, i) {
         setTimeout(function() {
-          //story.move();
+          move(story);
         }, config.movement._entry.delay*i)
       })
 
@@ -259,6 +268,8 @@
       let classCounter = 0;
 
       stories.forEach(function(item) {
+        animations.stopMovement(item);
+
         if(item.display) {
           if(classCounter % 2 != 0) {
             $(item.target).removeClass('left');
@@ -335,6 +346,7 @@
     let x = randomBetween(from, to);
 
     if(checkOverlap) {
+      console.log(from, to);
       var counter = 0;
       while(x < 40 && (x == 0 || isOverlapingX(x))) {
         x = randomBetween(from, to);
@@ -395,7 +407,8 @@
     }, 100);
 
     setTimeout(() => {
-      animations.makeElMove(el);
+      if(getDevice() == 'desktop')
+        animations.makeElMove(el);
     }, 1000)
 
     return el;
@@ -653,7 +666,8 @@
         let $item = $(e.target).closest('.item');
         let item = findStory($item.data('id'));
 
-        animations.makeElMove(item);
+        if(getDevice() == 'desktop')
+          animations.makeElMove(item);
       })
       .click(function(e) {                                        // __click event
         openModal($(e.target).data('id'));
@@ -831,9 +845,10 @@
           }
        },
        change: function( event, ui ) {
-        stories.forEach(function(story) {
-          animations.makeElMove(story);
-        })
+        if(getDevice == 'desktop')
+          stories.forEach(function(story) {
+            animations.makeElMove(story);
+          })
 
         smallItems.forEach(function(el) {
           animations.stopMovement(el);
@@ -946,30 +961,15 @@
 
   // on window resize, update config
   $(window).resize(function()  {
-    if(device != getDevice()) {
-      //console.log("SWITCH TO " + getDevice())
-      device = getDevice();
-
-
-
-      if(device == 'mobile') {
-
-      } else if(device == 'tablet') {
-        
-
-      } else {
-        //$('.filter-wrapper').slick('unslick');
-      }
-    }
-
-
     let width = 0;
-    
     $('.cat-item').toArray().forEach(function(item) {
       width += $(item).outerWidth();
     })
 
-    if(width > $('.filter-wrapper').width()) {
+
+
+    if(width > $('.filter-wrapper').width() && $('.filter-wrapper.slick-slider').length == 0) {
+
       $('.filter-wrapper').slick({
         arrows: false,
         infinite: false,
@@ -979,11 +979,20 @@
         getStoriesReady();
         $('.selected').text("A-Z");
       })
-    } else {
-      $('.filter-wrapper').slick('unslick');
-    }
-    
 
+    } else if(width < $('.filter-wrapper').width() && $('.filter-wrapper.slick-slider').length > 0) { 
+        $('.filter-wrapper').slick('unslick'); 
+    }
+      
+
+
+
+    if(device != getDevice()) {
+      device = getDevice();
+      
+      getStoriesReady();
+      
+    }
     config._width = $board.width();
     config._height = $board.height();
   });
@@ -1054,7 +1063,6 @@
 
 
   $('.container-small').on('mouseenter', function() {
-    console.log("REMOVE!!");
     $('.item').removeClass('hovered');
   })
 })(jQuery, Drupal);
