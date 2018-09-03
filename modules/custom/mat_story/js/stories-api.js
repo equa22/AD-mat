c0f1ac0ebe7b5474f7a9ce950a22e7513e6cf1f5
Sara Verhnjak <sara.verhnjak@agiledrop.com>
2018-09-03 15:30:40 +0200

    New filtering, hover shadow, etc...

--- modules/custom/mat_story/js/stories-api.js
+++ modules/custom/mat_story/js/stories-api.js
@@ -37,7 +37,6 @@
     }
   };
 
-
   var selectedCategory = null;
   var selectedLetter = null;
 
@@ -53,16 +52,16 @@
 
   var device = getDevice();
 
-  function checkMobile() {
+  function mobile() {
     /*var check = false;
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
     return check;*/
-    if($(window).width() < 415) {
+    if($(window).width() < 991) {
       return true;
     } else
       return false;
   };
-  var mobile = checkMobile();
+  
   var animations = {
     //done: true,
     animateBubble: function(el)  {
@@ -318,7 +317,6 @@
     return arr;
   }
 
-
   function getTransition(speed) {
     return (
       'transform ' + speed + 'ms linear 0ms, ' +
@@ -372,25 +370,28 @@
       if(el.movement)
           animations.stopMovement(el);
 
-      el.speed = Math.abs(value) > 100 ? Math.abs(value) + 100 : 100;
+      el.speed = Math.abs(value) > 100 ? config.movement._smooth.speed : 100;
       getCss(el);
     })
 
     smallItems.forEach(function(el) {
       el.position.x = el.position.x + value/2;
       
-
       animations.stopMovement(el);
       if(el.position.x > config._width + 60) {
         el.position.x = -60;
         el.position.y = randomBetween(config._el_height, config._height);
         el.speed = 0;
         getCss(el);
+
+        //el.position.x = randomBetween(0, config._width);
       } else if(el.position.x < -60) {
         el.position.x = config._width + 60;
         el.position.y = randomBetween(config._el_height, config._height);
         el.speed = 0;
         getCss(el);
+
+        //el.position.x = randomBetween(0, config._width);
       }
       else {
         el.speed = Math.abs(value) > 100 ? config.background.image_bubbles._speed : 100;
@@ -500,7 +501,7 @@
         $('.item').removeClass('hovered');
 
         setTimeout(function() { $(e.target).addClass('hovered');}, 50);
-        if(mobile) return;
+        if(mobile()) return;
 
         var item = findStory($(e.target).data('id'));
         animations.stopMovement(item);
@@ -543,6 +544,59 @@
     getFilters();
   }
 
+
+  function addLettersFilter (arr) {
+    var letters = [];
+    $('.dropdown .dropdown-inner').html('');
+
+    arr.forEach(function(story) {
+      var first = false, last = false;
+      letters.forEach(function(letter) {
+        if(story.last_name[0].toUpperCase() == letter) {
+          last = true;
+        }
+        if(story.first_name[0].toUpperCase() == letter) {
+          first = true;
+        }
+      })
+      if(!last) {letters.push(story.last_name[0].toUpperCase())}
+      if(!first) {letters.push(story.first_name[0].toUpperCase())}
+    }) 
+    letters = letters.sort();
+
+    $(('<div/>'), {
+      'text': 'All',
+      'class': 'option all',
+      'css': {
+        'display': 'none'
+      }
+    }).appendTo('.dropdown .dropdown-inner').click(function(e) {
+      $('.selected').text("A - Z");
+      $('.option.all').css('display', 'none');
+      $('.option').removeClass('active');
+
+      selectedLetter = null;
+      getDisplayedStories();
+    });
+
+    letters.forEach(function(letter) {
+      $(('<div/>'), {
+        'text': letter,
+        'class': 'cat-item',
+        'data-letter': letter,
+        'class': 'option'
+      }).appendTo('.dropdown .dropdown-inner').click(function(e) {
+        $('.selected').text($(e.target).data('letter'));
+        $('.option.all').css('display', 'flex');
+
+        $('.option').removeClass('active');
+        $(e.target).addClass('active');
+        selectedLetter = $(e.target).data('letter');
+        getDisplayedStories();
+      })
+    })
+  }
+
   function getFilters() {
     var $filters = $('.filter-wrapper');
     var letters = [], categories = [];
@@ -582,6 +636,7 @@
       selectedLetter = null;
       selectedCategory = null;
       getDisplayedStories();
+      addLettersFilter(stories);
     })
     // append all categories to filter wrapper
     categories.forEach(function(category, i) {
@@ -594,6 +649,9 @@
       }).appendTo('.filter-wrapper').click(function(e) {
         $('.cat-item').removeClass('active');
         $(e.target).addClass('active');
+        // reset letter filter
+        $('.selected').text("A-Z");
+        selectedLetter = null;
 
         var filtered = [];
         stories.forEach(function(item) {
@@ -602,7 +660,7 @@
           }
         })
 
-        if(mobile) {
+        if(mobile()) {
           var slider = $( '.filter-wrapper' );
           slider[0].slick.slickGoTo(parseInt($(e.target).data('slick-index')));
 
@@ -613,24 +671,13 @@
 
         selectedCategory = $(e.target).data('category-id');
         getDisplayedStories();
+        addLettersFilter(displayedStories);
       })
     });
 
-    letters.forEach(function(letter) {
-      $(('<div/>'), {
-        'text': letter,
-        'class': 'cat-item',
-        'data-letter': letter,
-        'class': 'option'
-      }).appendTo('.dropdown .dropdown-inner').click(function(e) {
-        $('.selected').text($(e.target).data('letter'));
+    addLettersFilter(stories);
 
-        selectedLetter = $(e.target).data('letter');
-        getDisplayedStories();
-      })
-    })
-
-    if(mobile) {
+    if(mobile()) {
       appendSlick();
     }
   }
