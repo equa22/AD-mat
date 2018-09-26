9e26ed315e39d3f28adb8baf17547ba81464bc8b
JHacin <jan.hacin@agiledrop.com>
2018-09-26 15:12:34 +0200

    Allow editors to change the introduction text @ story submission form page.

--- modules/custom/mat_story/src/Form/StoryConfigForm.php
+++ modules/custom/mat_story/src/Form/StoryConfigForm.php
@@ -22,6 +22,8 @@ class StoryConfigForm extends ConfigFormBase {
     $config = $this->config('mat_story.settings');
     $user_content_value = $config->get('mat_story.user_content');
     $story_submitted = $config->get('mat_story.story_submitted');
+    $story_submission_introduction = $config->get('mat_story.story_submission_introduction');
+
     $form['title_admin'] = array(
       '#type' => 'inline_template',
       '#template' => '<h2>Admin Config</h2><p>Add recipient email addresses, separated with commas as shown below:<br> John Doe &lt;john.doe@domain.com&gt;, Mike Smith &lt;mike.smith@domain.com&gt;'
@@ -54,6 +56,15 @@ class StoryConfigForm extends ConfigFormBase {
       '#required' => TRUE
     );
 
+    $form['story_submission_introduction'] = array(
+      '#type' => 'text_format',
+      '#title' => $this->t('Introduction to the Story Submission form'),
+      '#description' => $this->t('Text displayed at the top of the Story Submission page.'),
+      '#default_value' => $story_submission_introduction['value'],
+      '#format' => 'rich_editor',
+      '#required' => TRUE
+    );
+
     $form['story_submitted'] = array(
       '#type' => 'text_format',
       '#title' => $this->t('Confirmation message'),
@@ -76,7 +87,8 @@ class StoryConfigForm extends ConfigFormBase {
     $config->set('mat_story.user_title', $form_state->getValue('user_title'));
     $config->set('mat_story.user_content', $form_state->getValue('user_content'));
     $config->set('mat_story.story_submitted', $form_state->getValue('story_submitted'));
- 
+    $config->set('mat_story.story_submission_introduction', $form_state->getValue('story_submission_introduction'));
+
     $config->save();
  
     return parent::submitForm($form, $form_state);
